0fc4c415b18bab889a2924530d85a041a51787b2
Avenir Baryshev <avenirpik@gmail.com>
2025-06-14 19:21:14 +0700

    2 - going

--- 13_2/Program.cs
+++ 13_2/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace _13_2
+﻿using System.Globalization;
+using System.Security.AccessControl;
+
+namespace _13_2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
+
+
+    public class SmartDevice
+    {
+        public string deviceName { get; }
+        public string deviceStatus { get; set; } = "Статус не инициализирован";
+        public SmartDevice(string deviceName)
+        {
+            this.deviceName = deviceName;
+        }
+        public override string ToString()
+        {
+            return (deviceName + " : " + deviceStatus);
+        }
+    }
+    internal class SmartHome
+    {
+
+        SmartDevice sd1 = new SmartDevice("Дверь");
+        void LockDoor() { sd1.deviceStatus = "Закрыта"; }
+        void UnlockDoor() { sd1.deviceStatus = "Открыта"; }
+
+        SmartDevice sd2 = new SmartDevice("Термостат");
+        void SetTemperature(int temperature) { sd2.deviceStatus = $"Установлено {temperature} градусов"; }
+
+        SmartDevice sd3 = new SmartDevice("Свет");
+        void TurnOnLight() { sd3.deviceStatus = "Включен"; }
+        void TurnOffLight() { sd3.deviceStatus = "Выключен"; }
+
+    }
+
 }
